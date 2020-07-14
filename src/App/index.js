cccdb51949b0d627006a025023383d32576cb5ea
Ernesto Daset <ernesto@MacBook-Pro.local>
2020-07-14 10:00:03 -0400

    Add Appprovider and hook up to AppBar

---
+++ src/App/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "./App.css";
+import WelcomeMessage from "./WelcomeMessage";
+import AppLayout from "./AppLayout";
+import AppBar from "./AppBar";
+import { AppProvider } from "./AppProvider";
+
+function App() {
+  return (
+    <AppLayout>
+      <AppProvider>
+        <AppBar />
+        <WelcomeMessage />
+      </AppProvider>
+    </AppLayout>
+  );
+}
+
+export default App;
