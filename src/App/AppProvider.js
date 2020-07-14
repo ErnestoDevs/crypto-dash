bf204e7dbc5b53ccec7a76719841e87cd22ff1b3
Ernesto Daset <ernesto@MacBook-Pro.lan1>
2020-07-14 12:38:26 -0400

    settings page localStorage

--- src/App/AppProvider.js
+++ src/App/AppProvider.js
@@ -2,17 +2,37 @@ import React from "react";
 
 export const AppContext = React.createContext();
 
-const MAX_FAVORITES = 10;
-const TIME_UNITS = 10;
-
 export class AppProvider extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      page: "settings",
+      page: "dashboard",
+      ...this.savedSettings(),
       setPage: this.setPage,
+      confirmFavorites: this.confirmFavorites,
     };
   }
+  confirmFavorites = () => {
+    this.setState({
+      firstVisit: false,
+      page: "dashboard",
+    });
+    localStorage.setItem(
+      "cryptoDash",
+      JSON.stringify({
+        test: "hello",
+      })
+    );
+  };
+
+  savedSettings() {
+    let cryptoDashData = JSON.parse(localStorage.getItem("cryptoDash"));
+    if (!cryptoDashData) {
+      return { page: "settings", firstVisit: true };
+    }
+    return {};
+  }
+
   setPage = (page) => this.setState({ page });
 
   render() {
--- src/App/index.js
+++ src/App/index.js
@@ -1,19 +1,21 @@
 import React, { Component } from "react";
-import "./App.css";
-import WelcomeMessage from "./WelcomeMessage";
+import WelcomeMessage from "../Settings/WelcomeMessage";
 import AppLayout from "./AppLayout";
 import AppBar from "./AppBar";
 import { AppProvider } from "./AppProvider";
+import Settings from "../Settings";
 
-function App() {
-  return (
-    <AppLayout>
-      <AppProvider>
-        <AppBar />
-        <WelcomeMessage />
-      </AppProvider>
-    </AppLayout>
-  );
+class App extends Component {
+  render() {
+    return (
+      <AppLayout>
+        <AppProvider>
+          <AppBar />
+          <Settings />
+        </AppProvider>
+      </AppLayout>
+    );
+  }
 }
 
 export default App;
